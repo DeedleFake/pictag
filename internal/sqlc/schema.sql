34d3b8e7246077438e05fee426e46d217e64a9dc
DeedleFake <deedlefake@users.noreply.github.com>
2025-04-22 10:06:47 -0400

    internal/sqlc: add `IF NOT EXISTS` to stuff in the schema

--- internal/sqlc/schema.sql
+++ internal/sqlc/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE images (
+CREATE TABLE IF NOT EXISTS images (
 	id TEXT PRIMARY KEY,
 	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	image_created_at DATETIME NOT NULL
 );
 
-CREATE TABLE tags (
+CREATE TABLE IF NOT EXISTS tags (
 	id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	image_id TEXT NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE tags (
 	FOREIGN KEY(image_id) REFERENCES images(id)
 );
 
-CREATE UNIQUE INDEX tags_name_image_id ON tags (name, image_id);
-CREATE UNIQUE INDEX tags_image_id_name ON tags (image_id, name);
-CREATE INDEX tags_name ON tags (name);
-CREATE INDEX tags_image_id ON tags (image_id);
+CREATE UNIQUE INDEX IF NOT EXISTS tags_name_image_id ON tags (name, image_id);
+CREATE UNIQUE INDEX IF NOT EXISTS tags_image_id_name ON tags (image_id, name);
+CREATE INDEX IF NOT EXISTS tags_name ON tags (name);
+CREATE INDEX IF NOT EXISTS tags_image_id ON tags (image_id);
