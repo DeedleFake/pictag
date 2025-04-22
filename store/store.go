d8c1a769b7512eaf0d11769f798a00f768960f07
DeedleFake <deedlefake@users.noreply.github.com>
2025-04-21 20:09:28 -0400

    store: godoc

--- store/store.go
+++ store/store.go
@@ -15,10 +15,18 @@ import (
 
 var ErrInvalidName = errors.New("invalid name")
 
+// Store is a hash-addressed on-disk storage system for images. It
+// converts images to a standard format and writes them to disk,
+// deduplicating them by the hash of the resulting data. It can then
+// retreive those images by identifying them by the resulting hash.
 type Store struct {
 	root *os.Root
 }
 
+// Open opens a store rooted at path. The directory at path must
+// already exist. No checks are done for data in an existing
+// directory, so the store will just write data right alongside
+// anything unrelated that is already there.
 func Open(path string) (*Store, error) {
 	root, err := os.OpenRoot(path)
 	if err != nil {
@@ -30,6 +38,7 @@ func Open(path string) (*Store, error) {
 	}, nil
 }
 
+// Close closes the store.
 func (s *Store) Close() error {
 	return s.root.Close()
 }
@@ -51,6 +60,7 @@ func (s *Store) store(name string, data []byte) error {
 	return err
 }
 
+// Store stores the given image into s, returning its identifying hash.
 func (s *Store) Store(img image.Image) (string, error) {
 	h := sha256.New()
 	var buf bytes.Buffer // TODO: Move this to the disk in case it's huge?
@@ -64,7 +74,9 @@ func (s *Store) Store(img image.Image) (string, error) {
 	return name, s.store(name, buf.Bytes())
 }
 
-func (s *Store) Open(name string) (image.Image, error) {
+// Load returns the image identified by name, if it exists. If name is
+// invalid, an error equal to ErrInvalidName is returned.
+func (s *Store) Load(name string) (image.Image, error) {
 	if len(name) != 64 {
 		return nil, fmt.Errorf("%w: %q", ErrInvalidName, name)
 	}
