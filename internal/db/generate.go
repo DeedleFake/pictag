8fcffdf74585ee81aa979d090fa307af76d24e56
DeedleFake <deedlefake@users.noreply.github.com>
2025-04-21 23:18:07 -0400

    internal/db: add `Init()`

--- internal/db/generate.go
+++ internal/db/generate.go
@@ -1,2 +1,16 @@
 //go:generate go tool github.com/sqlc-dev/sqlc/cmd/sqlc generate
 package db
+
+import (
+	"context"
+	_ "embed"
+)
+
+//go:embed schema.sql
+var schema string
+
+// Init initializes a database with the schema.
+func Init(ctx context.Context, db DBTX) error {
+	_, err := db.ExecContext(ctx, schema)
+	return err
+}
