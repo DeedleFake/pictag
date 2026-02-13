e678e5fbb183e935808ebbdebfaf177396154e91
DeedleFake <deedlefake@users.noreply.github.com>
2026-02-13 10:36:48 -0500

    ui: use a build script and `react-select`

---
+++ build.ts
@@ -0,0 +1,15 @@
+import tailwind from 'bun-plugin-tailwind'
+
+const result = await Bun.build({
+  outdir: './internal/ui/assets',
+  minify: true,
+  sourcemap: 'linked',
+  naming: {
+    chunk: '[name].[ext]',
+    asset: '[name].[ext]',
+  },
+  entrypoints: ['./ui/index.html'],
+  plugins: [tailwind],
+})
+
+console.log(result)
--- ui/App.tsx
+++ ui/App.tsx
@@ -1,35 +1,23 @@
-import { useMemo, useState } from 'react'
 import { BrowserRouter, useSearchParams } from 'react-router'
+import AsyncSelect from 'react-select/async'
 
 import './index.css'
 
-function splitTags(q: string) {
-  return Array.from(new Set(q.split(/\s+/).filter((tag) => tag !== '')))
+async function loadTags(input: string) {
+  return []
 }
 
 function Search() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const tags = useMemo(
-    () => Array.from(new Set(searchParams.getAll('q'))),
-    [searchParams],
-  )
-  const [search, setSearch] = useState(tags.join(' '))
 
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={(ev) => {
-          setSearch(ev.target.value)
-          setSearchParams({ q: splitTags(ev.target.value) })
-        }}
+    <div className="flex flex-col justify-start items-center">
+      <AsyncSelect
+        className="w-52"
+        isMulti
+        defaultOptions
+        loadOptions={loadTags}
       />
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
     </div>
   )
 }
